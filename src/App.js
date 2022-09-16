3b26c16825468eb2f6a2fac045332ae624df765a
maksimjeet20566 <98419737+MakC-Ukr@users.noreply.github.com>
2022-09-16 19:22:29 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, { useState, useEffect } from "react";
 import logo from './logo.svg';
 import './App.css'
-import axios from "axios";
-import firebaseApp from './initFirebase'
 const fetch = require('node-fetch');
 
 
 function App() {
   const [linkData, setLinkData] = useState("https://google.com");
   const getNames = () => {
-    fetch('https://raw.githubusercontent.com/MakC-Ukr/landing-page/master/src/link.txt')
+    fetch('https://raw.githubusercontent.com/tempmaksimjeet/question-duplic-landing-page/main/src/link.txt')
       .then(res => res.text()).then(data => {
         setLinkData(data);
       }).catch(err => console.log('fetch error', err));
